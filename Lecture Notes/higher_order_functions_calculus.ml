085591de82cf122422991100f1642261f8b2b236
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-02-08 17:40:38 -0500

    Update higher_order_functions_calculus.ml

--- Lecture Notes/higher_order_functions_calculus.ml
+++ Lecture Notes/higher_order_functions_calculus.ml
@@ -28,3 +28,27 @@ let r_sq (x:float):float = x *. x;;
 integral(r_sq,0.0,1.0,0.001);;
 
 integral(sin,0.0, 3.14159, 0.001);;
+
+
+
+(* No type declarations *)
+let deriv(f, dx) = fun x -> ((f(x +. dx) -. f(x))/.dx);;
+
+let absFl x = if (x < 0.0) then -.x else x;; 
+
+let iterSum(f, lo, hi, inc) =
+  let rec helper(x, result) =
+    if (x > hi) then result
+    else helper((inc x), (f x) +. result)
+  in
+  helper(lo,0.0);;
+
+let integral(f,lo,hi,dx) =
+  let delta x = x +. dx in
+  dx *. iterSum(f,(lo +. (dx/.2.0)), hi, delta);;
+
+let r_sq x = x *. x;;
+
+integral(r_sq,0.0,1.0,0.001);;
+
+integral(sin,0.0, 3.14159, 0.001);;
