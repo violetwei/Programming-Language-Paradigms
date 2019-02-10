0a6bef3effdf32dbf9497e0e59285bcfe740ae9f
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-02-10 14:16:36 -0500

    Create tricky_examples_with_lists.ml

---
+++ Lecture Notes/tricky_examples_with_lists.ml
@@ -0,0 +1,21 @@
+let psums lst =
+  let rec helper l a =
+    match l with
+      | [] -> [a]
+      | x ::xs -> a :: (helper xs (x + a))
+  in
+  match lst with
+    | [] -> [0]
+    | x :: xs -> helper xs x;;
+
+let smash ll = List.fold_left (@) [] ll;;
+
+let rec inter item lst =
+  match lst with
+    | [] -> [[item]]
+    | x :: xs -> (item :: lst) :: (List.map (fun u -> (x:: u)) (inter item xs));;
+
+let rec perms l =
+  match l with
+    | [] -> [[]]
+    | x::xs -> smash (List.map (fun u -> (inter x u)) (perms xs));;
