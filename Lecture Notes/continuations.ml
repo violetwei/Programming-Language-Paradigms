bb819ad3dbe93dd9f13a32d78f40333a1afebfe3
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-04-03 22:40:20 -0400

    Create continuations.ml

---
+++ Lecture Notes/continuations.ml
@@ -0,0 +1,13 @@
+(* Regular, non-tail recursive append *)
+let rec append (l1 : 'a list) (l2 : 'a list) : 'a list =
+  match l1 with
+  | [] -> l2
+  | x :: xs ->
+     let r = append xs l2 in
+     x :: r
+
+let rec append_k (l1 : 'a list) (l2 : 'a list) (k : 'a list -> 'r) : 'r =
+  match l1 with
+  | [] -> k l2
+  | x :: xs ->
+     append_k xs l2 (fun ys -> k (x :: ys))
