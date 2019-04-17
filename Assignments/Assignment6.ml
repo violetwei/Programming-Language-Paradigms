f5d0fcafdfe85cc7e78e657bb6ae3b8a34466bf9
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-04-17 12:49:56 -0400

    Solutions updated

--- Assignments/Assignment6.ml
+++ Assignments/Assignment6.ml
@@ -51,3 +51,34 @@
   # alison("MathIsTheBest", CheckBalance);;
   Account closed.
   - : unit = ()
+  
+  
+(* Solution to Question 1 *)
+
+let makeProtectedAccount((openingBalance: int), (password: string)) =
+    let isOpen = ref true in 
+    let balance = ref openingBalance in 
+    let passwd = ref password in 
+    fun ((passkey:string), (t : transaction)) ->
+      if not !isOpen 
+      then 
+        print_string "Account closed."
+      else
+        if not (passkey = !passwd) then
+          print_string "Incorrect password."
+        else
+          match t with
+          | Withdraw(x) ->
+             if (x < !balance)
+             then
+               (balance := !balance - x;
+               Printf.printf "The new balance is: %i." !balance)
+            else
+              print_string "Insufficient funds."
+          | Deposit(x) ->
+              balance := !balance + x;
+              Printf.printf "The new balance is: %i." !balance
+          | CheckBalance ->
+              Printf.printf  "The balance is: %i." !balance
+          | ChangePassword newPwd -> (passwd := newPwd; print_string "Password changed.")
+          | Close -> isOpen := false; print_string "Account successfully closed.";;  
