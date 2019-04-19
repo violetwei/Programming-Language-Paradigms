79aaf9280caca7fe9cfffb4098b7340c2d2dc96b
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-04-18 23:01:27 -0400

    Imperative Programming Examples added

--- Lecture Notes/imperativeProgramming_Refs.ml
+++ Lecture Notes/imperativeProgramming_Refs.ml
@@ -27,3 +27,23 @@ Array.set ar2 8 "dog.";;
 for i = 0 to ((Array.length ar2) - 1) do
   (print_string (Array.get ar2 i); print_string " ")
 done;;
+
+(* Imperative Programming Examples *)
+let x = ref 1;;
+
+let mash (n:int) =
+  let m = ref n in
+  (Printf.printf "m is %i " !m);(m := !m + 1); (Printf.printf "n is %i " n); 
+  (Printf.printf "m is %i" !m);;
+
+
+ let foo n =
+  let x = ref n in     
+  while (!x < 10) do
+    (Printf.printf "x is %i\n " !x);(x := !x + 1)
+  done;;
+
+type person = { name : string ; birthday : int * int; title : string };;
+
+let prakash = { name = "Prakash"; birthday = (3,11); 
+                title = "Bane of while loops"};;
